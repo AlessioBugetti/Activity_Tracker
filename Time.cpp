b9cac1ae177cf340c41acbb1f47fa998b1bed3f8
Alessio <alessio.bugetti@stud.unifi.it>
2019-05-24 17:34:43 +0200

    gestione eccezioni classe Time

--- Time.cpp
+++ Time.cpp
@@ -2,13 +2,14 @@
 // Created by alessio on 23/05/19.
 //
 
+#include <stdexcept>
 #include "Time.h"
 
 Time::Time(int h, int m) : hours(h), minutes(m){
     if (h >= 24 || h<0)
-        hours=0;
+        throw std::domain_error("ore non valide");
     if( m>59 || m<0)
-        minutes=50;
+        throw std::domain_error("minuti non validi");
 }
 
 int Time::getHours() const {
