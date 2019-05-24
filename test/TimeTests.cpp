4c794b3382d7fcc98ccc53dabac516e624f813f4
Alessio <alessio.bugetti@stud.unifi.it>
2019-05-24 18:03:34 +0200

    aggiunta di test sulle eccezioni per la classe Time

--- test/TimeTests.cpp
+++ test/TimeTests.cpp
@@ -11,3 +11,11 @@ TEST(Time, TimeConstructor) {
     ASSERT_EQ(10, t.getHours());
     ASSERT_EQ(15, t.getMinutes());
 }
+
+TEST(Time, InvalidHour) {
+    ASSERT_THROW(Time t(30,10),std::domain_error);
+}
+
+TEST(Time, InvalidMinutes) {
+    ASSERT_THROW(Time t(10,70),std::domain_error);
+}
